76cd0ea7b576bbdfbb4bd734b9b799f0c1b1f799
JacobChan182 <jacobchanemail@gmail.com>
2025-10-01 20:48:12 -0400

    error cleanup (unused import)

--- src/pages/fun-corner.js
+++ src/pages/fun-corner.js
@@ -1,5 +1,5 @@
 import Footer from '../components/Footer';
-import FullPageScroll from '../components/portfolioHome/PortfolioScroll';
+//import FullPageScroll from '../components/portfolioHome/PortfolioScroll';
 import WorkInProgress from '../components/funCorner/WorkInProgress';
 export default function FunCorner () {
     return (
