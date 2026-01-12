b326a5e78c63923b492f7972531f0de79205cc62
JacobChan182 <jacobchanemail@gmail.com>
2026-01-12 02:19:17 -0500

    updated pages

--- src/pages/fun-corner.js
+++ src/pages/fun-corner.js
@@ -1,10 +1,8 @@
 import WorkInProgress from '../components/funCorner/WorkInProgress';
-import CounterStrikeStats from '../components/funCorner/CounterStrikeStats';
 
 export default function FunCorner () {
     return (
     <div>
-      <CounterStrikeStats />
       <WorkInProgress />
     </div>
   );
